e6badebbb0e20464d351fa4d642c97f59033442d
Micheal Ellis <mellis@erieconstruction.net>
2022-03-04 16:57:40 -0500

    finished 6.4.6 :q :! :wq :q q q :q Q:] Gq exit

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,9 +5,14 @@ var repoNameEl = document.querySelector("#repo-name");
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    //console.log(repoName);
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName; 
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("../../index.html");
+    }
 }
 
 var getRepoIssues = function(repo){
@@ -27,7 +32,7 @@ var getRepoIssues = function(repo){
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("../../index.html");
         }
     });
 };
