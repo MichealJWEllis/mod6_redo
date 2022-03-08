499c668b818efc6a7d465d81dd3e515f0037bf2d
Micheal Ellis <mellis@erieconstruction.net>
2022-03-08 15:57:59 -0500

    uhh

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,20 +1,36 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
+    // prevent page from refreshing
     event.preventDefault();
+
     // get value from input element
     var username = nameInputEl.value.trim();
 
     if (username) {
         getUserRepos(username);
-        nameInputEl.value = "";
+
+        // clear old content
         repoContainerEl.textContent = "";
+        nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
+    }
+};
+
+var buttonClickHandler = function (event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
     }
 };
 
@@ -58,36 +74,24 @@ var getFeaturedRepos = function (language) {
     });
 };
 
-
-var buttonClickHandler = function (event) {
-    // get the language attribute from the clicked element
-    var language = event.target.getAttribute("data-language");
-
-    if (language) {
-        getFeaturedRepos(language);
-
-        // clear old content
-        repoContainerEl.textContent = "";
-    }
-};
-
 var displayRepos = function (repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
-        repoContainerEl.textContent = "No Repositories found.";
+        repoContainerEl.textContent = "No repositories found.";
         return;
-    };
-    repoContainerEl.textContent = "";
+    }
+
     repoSearchTerm.textContent = searchTerm;
+
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
         // format repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name + "/" ;
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-        // create a container for each repo
+        // create a link for each repo
         var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "single-repo.html?repo=" + repoName);
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
@@ -100,21 +104,22 @@ var displayRepos = function (repos, searchTerm) {
         var statusEl = document.createElement("span");
         statusEl.classList = "flex-row align-center";
 
+        // check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
+
         // append to container
         repoEl.appendChild(statusEl);
 
-        // append container to the dom 
+        // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
-}
+};
 
-
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
 languageButtonsEl.addEventListener("click", buttonClickHandler);
-
-
