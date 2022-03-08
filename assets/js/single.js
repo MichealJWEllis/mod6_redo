ab10b169ef1b618ca977f926568ce9c6a9f38680
Micheal Ellis <mellis@erieconstruction.net>
2022-03-08 15:30:32 -0500

    redirect2

--- assets/js/single.js
+++ assets/js/single.js
@@ -11,7 +11,7 @@ var getRepoName = function() {
         getRepoIssues(repoName);
     } else {
         // if no repo was given, redirect to the homepage
-        document.location.replace("./index.html");
+        // document.location.replace("./index.html");
     }
 }
 
